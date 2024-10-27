2fec953ad6a737526cdf04bd77cda2663f9f15e2
Michael Behailu <105115697+Miki-b@users.noreply.github.com>
2024-10-27 11:12:22 +0300

    Time: 0 ms (100%), Space: 16.5 MB (40.37%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.py
@@ -7,8 +7,7 @@ class Solution:
             if s[i] not in sub and len(set(sub))==2:
                     count+=1      
             sub.append(s[i])
-            print(sub)
-            print(count)
+            
             if len(sub)>=3:
                 sub.remove(s[L])
                 L+=1
