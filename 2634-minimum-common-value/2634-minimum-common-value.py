4e7b4bcc78459387be01f40bc34cf2ad4f3792ba
Michael Behailu <105115697+Miki-b@users.noreply.github.com>
2024-12-03 00:18:49 +0300

    Time: 15 ms (30.92%), Space: 35.5 MB (37.33%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.py
+++ 2634-minimum-common-value/2634-minimum-common-value.py
@@ -1,34 +1,16 @@
-import bisect
 class Solution:
-    def search(self, haystack, needle, start=0):
-            find = bisect.bisect_left(haystack, needle, start, len(haystack) - 1)
-            if needle == haystack[find]:
-                return needle
-            else:
-                return -1
-                
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        # Edge case for 1 element in array
-        if len(nums1) == 1:
-            return nums1[0] if nums1[0] in nums2 else -1
-        if len(nums2) == 1:
-            return nums2[0] if nums2[0] in nums1 else -1
+        ans,i,j=0,0,0
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]==nums2[j]:
+                return nums1[i]
+            elif nums1[i]>nums2[j]:
+                j+=1
+            elif nums1[i]<nums2[j]:
+                i+=1
+            
+        return -1
 
-    
-        p1, p2 = 0,0
-        res = -1
-        while p1 < len(nums1) - 1 and p2 < len(nums2) - 1:
-            if nums1[p1] == nums2[p2]:
-                return nums1[p1]
-            elif nums1[p1] < nums2[p2]:
-                res = self.search(nums1, nums2[p2], p1+1)
-                if res != -1:
-                    return res
-                p2 += 1
-            else:
-                res = self.search(nums2, nums1[p1], p2+1)
-                if res != -1:
-                    return res
-                p1 += 1
+
         
-        return res
+        
