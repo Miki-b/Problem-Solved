f0ac28aa62a396e1d20adf711204bebfe6ba203d
Michael Behailu <105115697+Miki-b@users.noreply.github.com>
2024-12-03 01:42:01 +0300

    Time: 0 ms (100%), Space: 17.3 MB (6.85%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,29 +1,46 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        num1=0
-        num2=0
-        L1=len(a)
-        L2=len(b)
-        for i in a:
-            num1=num1+(int(i)*pow(2,L1-1))
-            L1=L1-1
-        for j in b:
-            num2=num2+(int(j)*pow(2,L2-1))
-            L2=L2-1
-        num3=num1+num2
-        ans=""
-        for k in range(num3+1):
-            if num3%2==0:
-                ans=ans+"0"
-                num3=num3//2
-            elif num3==1:
-                ans=ans+"1"
-                break
-            else:
-                ans=ans+"1"
-                num3=num3//2
-        ans2=""
-        for l in range(1,len(ans)+1):
-            ans2=ans2+ans[-l]
-        return ans2
+        carry = "0"
+        ans = []
+
+        def calculator(d: str, e: str, f: str) -> str:
+            nonlocal carry
+            if d == "0" and e == "0" and f == "0":
+                carry = "0"
+                return "0"
+            elif d == "0" and e == "0" and f == "1":
+                carry = "0"
+                return "1"
+            elif d == "0" and e == "1" and f == "0":
+                carry = "0"
+                return "1"
+            elif d == "0" and e == "1" and f == "1":
+                carry = "1"
+                return "0"
+            elif d == "1" and e == "0" and f == "0":
+                carry = "0"
+                return "1"
+            elif d == "1" and e == "0" and f == "1":
+                carry = "1"
+                return "0"
+            elif d == "1" and e == "1" and f == "0":
+                carry = "1"
+                return "0"
+            elif d == "1" and e == "1" and f == "1":
+                carry = "1"
+                return "1"
+            return "0"
+
+        i, j = len(a) - 1, len(b) - 1
+        while i >= 0 or j >= 0 or carry == "1":
+            x = a[i] if i >= 0 else "0"
+            y = b[j] if j >= 0 else "0"
+            ans.append(calculator(carry, x, y))
+            i -= 1
+            j -= 1
+        if carry == "1":
+            ans.append(carry)
+
+        return "".join(reversed(ans))
+
 
