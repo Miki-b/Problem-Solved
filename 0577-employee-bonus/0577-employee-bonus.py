a8d60a3305ddaaaa399efb774162e0b847d4ca56
Michael Behailu <michaelbehailu0@gmail.com>
2026-05-24 01:27:34 +0300

    Time: 308 ms (95.27%), Space: 68.6 MB (23.33%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    result = pd.merge(employee, bonus, on='empId', how='left')
+
+    result = result[
+        (result['bonus'] < 1000) | (result['bonus'].isna())
+    ]
+
+    return result[['name', 'bonus']]
